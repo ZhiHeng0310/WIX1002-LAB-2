7a5618530f54c44d95dd630faec3708366a19c47
Zhi Heng <25006645@siswa.um.edu.my>
2025-11-11 23:27:31 +0800

    Update TODO comment in celsius.java

--- L2Q1/src/l2q1/celsius.java
+++ L2Q1/src/l2q1/celsius.java
@@ -14,7 +14,7 @@ public class celsius {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here lol
         Scanner input = new Scanner(System.in);
         System.out.print("Enter temperature in degree Fahrenheit:");
         double temperature = input.nextDouble();
